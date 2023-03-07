b6ebf3d7e31e29fc6436b5ca29e5684ed1f78268
alswn <alswn9938@gmail.com>
2023-03-07 16:51:47 +0900

    add(exception) : Exception Entity 정의

---
+++ back/src/main/java/com/hanwul/kbscbackend/ex/common/ExceptionResponse.java
@@ -0,0 +1,15 @@
+package com.hanwul.kbscbackend.ex.common;
+
+public class ExceptionResponse {
+    private String errorCode;
+    private String errorCodePhrase;
+    private String errorHint;
+    private String errorMessage;
+
+    public ExceptionResponse(int errorCode, String errorCodePhrase, String errorHint, String errorMessage) {
+        this.errorCode = String.valueOf(errorCode);
+        this.errorCodePhrase = errorCodePhrase;
+        this.errorHint = errorHint;
+        this.errorMessage = errorMessage;
+    }
+}
