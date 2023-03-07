f89204e5836b96772687c5f409fb6d81dc764f1a
alswn <alswn9938@gmail.com>
2023-03-07 18:43:58 +0900

    refactor(exception) : ExceptionResponse 필드명 변경
    errorHint -> errorOccurrencePackage
    에러가 발생한 코드가 속한 패키지명을 알려주기 위함

--- back/src/main/java/com/hanwul/kbscbackend/exception/common/ExceptionResponse.java
+++ back/src/main/java/com/hanwul/kbscbackend/exception/common/ExceptionResponse.java
@@ -3,13 +3,13 @@ package com.hanwul.kbscbackend.exception.common;
 public class ExceptionResponse {
     private String errorCode;
     private String errorCodePhrase;
-    private String errorHint;
+    private String errorOccurrencePackage;
     private String errorMessage;
 
-    public ExceptionResponse(int errorCode, String errorCodePhrase, String errorHint, String errorMessage) {
+    public ExceptionResponse(int errorCode, String errorCodePhrase, String errorOccurrencePackage, String errorMessage) {
         this.errorCode = String.valueOf(errorCode);
         this.errorCodePhrase = errorCodePhrase;
-        this.errorHint = errorHint;
+        this.errorOccurrencePackage = errorOccurrencePackage;
         this.errorMessage = errorMessage;
     }
 }
