76a77b0edbeb514f65cfb7250b712d1f940dd778
alswn <alswn9938@gmail.com>
2023-03-09 21:02:27 +0900

    refactor(Exception) : AnswerService 수정

--- back/src/main/java/com/hanwul/kbscbackend/exception/common/DetailInformations.java
+++ back/src/main/java/com/hanwul/kbscbackend/exception/common/DetailInformations.java
@@ -3,7 +3,10 @@ package com.hanwul.kbscbackend.exception.common;
 public enum DetailInformations {
     DUPLICATED_USERNAME("중복된 user name"),
     UNKNOWN_USERNAME("등록되지 않은 user name"),
-    WRONG_PASSWORD("잘못된 password");
+    WRONG_PASSWORD("잘못된 password"),
+    QUESTION_EXCEPTION("Question 객체 오류"),
+    ANSWER_EXCEPTION("Answer 객체 오류");
+
 
 
     private String information;
